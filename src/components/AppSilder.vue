140aee5c6aabea1cdc22ff5f10fc2f6ef1133f96
1612519774 <1612519774@qq.com>
2019-07-08 15:29:53 +0800

    实现AppSilder.vue

--- src/components/AppSilder.vue
+++ src/components/AppSilder.vue
@@ -1,32 +1,25 @@
 <template>
-<!-- 侧边栏 -->
-   <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active">
-              <a href="#">
-                英雄列表
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">Reports</a>
-            </li>
-            <li>
-              <a href="#">Analytics</a>
-            </li>
-            <li>
-              <a href="#">Export</a>
-            </li>
-          </ul>
-        </div>
+  <!-- 侧边栏 -->
+  <div class="col-sm-3 col-md-2 sidebar">
+    <ul class="nav nav-sidebar">
+      <li class="active">
+        <a href="#">
+          英雄列表
+        </a>
+      </li>
+      <li>
+        <a href="#">武器列表</a>
+      </li>
+      <li>
+        <a href="#">装备列表</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
