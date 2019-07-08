18deba72010d41355e8b354c7fa914739bfbe776
1612519774 <1612519774@qq.com>
2019-07-08 15:55:09 +0800

    实现HeroList.vue

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HeroList from "./components/views/heroes/HeroList.vue"
+// 导入vue-router
 import VueRouter from 'vue-router'
 Vue.config.productionTip = false
-
+// 导入样式文件
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/css/index.css'
+// 配置vue的插件vue-router
 Vue.use(VueRouter);
 const router = new VueRouter({
+  // 配置路由规则
   routes: [
-
+    {name:"home",path:"/", redirect:"/heroes"},
+    {
+      name: "heroes", path: "/heroes", component: HeroList
+    }
 ]
 })
 new Vue({
   el: '#app',
   render: h => h(App),
+  // 配置路由对象
   router
 })
