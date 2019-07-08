fe8f5688ce62c7e16c225cd5567db7152040a25f
1612519774 <1612519774@qq.com>
2019-07-08 16:25:45 +0800

    武器列表和装备列表实现路由跳转

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import HeroList from "./components/views/heroes/HeroList.vue"
+import WeaponList from "./components/views/weapons/WeaponList.vue"
+import EquipList from "./components/views/equips/EquipList.vue"
 // 导入vue-router
 import VueRouter from 'vue-router'
 Vue.config.productionTip = false
 // 导入样式文件
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './assets/css/index.css'
+
 // 配置vue的插件vue-router
 Vue.use(VueRouter);
 const router = new VueRouter({
+   linkActiveClass: 'active',
   // 配置路由规则
   routes: [
     {name:"home",path:"/", redirect:"/heroes"},
     {
       name: "heroes", path: "/heroes", component: HeroList
-    }
+    },
+     {
+      name: "weapons", path: "/weapons", component: WeaponList
+    },
+    {
+      name: "equips", path: "/equips", component: EquipList
+    },
 ]
 })
 new Vue({
